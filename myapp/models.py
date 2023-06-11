36515528c73282380bf3304afbbf94bce1bf318c
nrjsharma <nrjsharma083@gmail.com>
2023-06-11 20:07:23 +0530

    Add Post Model and Done API setup

---
+++ myapp/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(User, related_name="posts", on_delete=models.CASCADE)
+    description = models.TextField()
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = "Post"
+        verbose_name_plural = "Posts"
+
+    def __str__(self):
+        """String for representing the IloveDjango object (in Admin site etc.)."""
+        return '%s - %s' % (self.id, self.title)
